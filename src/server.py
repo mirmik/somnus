cb2304d5564dc9672a4a0226fe2dd624dd2c5c86
mirmik <mirmikns@yandex.ru>
2023-08-28 18:18:39 +0300

    'a'

--- src/server.py
+++ src/server.py
@@ -9,10 +9,12 @@ import uuid
 import threading
 
 import cv2
+import numpy
+import math
 import aiohttp
 from aiohttp import web
 import aiortc
-from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
+from aiortc import MediaStreamTrack, VideoStreamTrack, RTCPeerConnection, RTCSessionDescription
 from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder, MediaRelay
 from av import VideoFrame
 
@@ -51,6 +53,67 @@ ndi_video_frame = ndi.VideoFrameV2()
 
 VIDEO_TRACKS = set()
 SERVER_MESSAGE_LISTENERS = set()
+
+class FlagVideoStreamTrack(VideoStreamTrack):
+    """
+    A video track that returns an animated flag.
+    """
+
+    def __init__(self):
+        super().__init__()  # don't forget this!
+        self.counter = 0
+        height, width = 480, 640
+
+        # generate flag
+        data_bgr = numpy.hstack(
+            [
+                self._create_rectangle(
+                    width=213, height=480, color=(255, 0, 0)
+                ),  # blue
+                self._create_rectangle(
+                    width=214, height=480, color=(255, 255, 255)
+                ),  # white
+                self._create_rectangle(width=213, height=480, color=(0, 0, 255)),  # red
+            ]
+        )
+
+        # shrink and center it
+        M = numpy.float32([[0.5, 0, width / 4], [0, 0.5, height / 4]])
+        data_bgr = cv2.warpAffine(data_bgr, M, (width, height))
+
+        # compute animation
+        omega = 2 * math.pi / height
+        id_x = numpy.tile(numpy.array(range(width), dtype=numpy.float32), (height, 1))
+        id_y = numpy.tile(
+            numpy.array(range(height), dtype=numpy.float32), (width, 1)
+        ).transpose()
+
+        self.frames = []
+        for k in range(30):
+            phase = 2 * k * math.pi / 30
+            map_x = id_x + 10 * numpy.cos(omega * id_x + phase)
+            map_y = id_y + 10 * numpy.sin(omega * id_x + phase)
+            self.frames.append(
+                VideoFrame.from_ndarray(
+                    cv2.remap(data_bgr, map_x, map_y, cv2.INTER_LINEAR), format="bgr24"
+                )
+            )
+
+    async def recv(self):
+        pts, time_base = await self.next_timestamp()
+
+        frame = self.frames[self.counter % 30]
+        frame.pts = pts
+        frame.time_base = time_base
+        self.counter += 1
+        return frame
+
+    def _create_rectangle(self, width, height, color):
+        data_bgr = numpy.zeros((height, width, 3), numpy.uint8)
+        data_bgr[:, :] = color
+        return data_bgr
+
+
 
 class VideoTransformTrack(MediaStreamTrack):
     """
@@ -135,10 +198,11 @@ async def offer(request):
 
     log_info("Created for %s", request.remote)
 
-
     @pc.on("datachannel")
     def on_datachannel(channel):
         on_datachannel_handler(channel)
+        #pc.addTrack(FlagVideoStreamTrack())
+    
 
     @pc.on("connectionstatechange")
     async def on_connectionstatechange():
@@ -175,8 +239,10 @@ async def offer(request):
         async def on_ended():
             log_info("Track %s ended", track.kind)
 
+    
     # handle offer
     await pc.setRemoteDescription(offer)
+
 
     # send answer
     answer = await pc.createAnswer()
