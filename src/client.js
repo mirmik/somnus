9fa05e587d9b46b73d5ed26b34a0e7225a928980
mirmik <mirmikns@yandex.ru>
2023-08-31 21:21:11 +0300

    'a'

--- src/client.js
+++ src/client.js
@@ -221,82 +221,82 @@ input.addEventListener("keypress", function(event) {
 
 
 
-  var ResolutionsToCheck = [
-    {width: 160, height:120},
-    {width: 320, height:180},
-    {width: 320, height:240},
-    {width: 640, height:360},
-    {width: 640, height:480},
-    {width: 768, height:576},
-    {width: 1024, height:576},
-    {width: 1280, height:720},
-    {width: 1280, height:768},
-    {width: 1280, height:800},
-    {width: 1280, height:900},
-    {width: 1280, height:1000},
-    {width: 1920, height:1080},
-    {width: 1920, height:1200},
-    {width: 2560, height:1440},
-    {width: 3840, height:2160},
-    {width: 4096, height:2160}
-];
+//   var ResolutionsToCheck = [
+//     {width: 160, height:120},
+//     {width: 320, height:180},
+//     {width: 320, height:240},
+//     {width: 640, height:360},
+//     {width: 640, height:480},
+//     {width: 768, height:576},
+//     {width: 1024, height:576},
+//     {width: 1280, height:720},
+//     {width: 1280, height:768},
+//     {width: 1280, height:800},
+//     {width: 1280, height:900},
+//     {width: 1280, height:1000},
+//     {width: 1920, height:1080},
+//     {width: 1920, height:1200},
+//     {width: 2560, height:1440},
+//     {width: 3840, height:2160},
+//     {width: 4096, height:2160}
+// ];
 
-var left = 0;
-var right = ResolutionsToCheck.length;
-var selectedWidth;
-var selectedHeight;
-var mid;
+// var left = 0;
+// var right = ResolutionsToCheck.length;
+// var selectedWidth;
+// var selectedHeight;
+// var mid;
 
-function FindMaximum_WidthHeight_ForCamera()
-{
-    //console.log("left:right = ", left, ":", right);
-    if(left > right)
-    {
-        console.log("Selected Height:Width = ", selectedWidth, ":", selectedHeight);
-        return;
-    }
+// function FindMaximum_WidthHeight_ForCamera()
+// {
+//     //console.log("left:right = ", left, ":", right);
+//     if(left > right)
+//     {
+//         console.log("Selected Height:Width = ", selectedWidth, ":", selectedHeight);
+//         return;
+//     }
 
-    mid = Math.floor((left + right) / 2);
+//     mid = Math.floor((left + right) / 2);
 
-    var temporaryConstraints = {
-        "audio": true,
-        "video": {
-        "mandatory": {
-            "minWidth": ResolutionsToCheck[mid].width,
-            "minHeight": ResolutionsToCheck[mid].height,
-            "maxWidth": ResolutionsToCheck[mid].width,
-            "maxHeight": ResolutionsToCheck[mid].height
-            },
-            "optional": []
-        }
-    }
+//     var temporaryConstraints = {
+//         "audio": true,
+//         "video": {
+//         "mandatory": {
+//             "minWidth": ResolutionsToCheck[mid].width,
+//             "minHeight": ResolutionsToCheck[mid].height,
+//             "maxWidth": ResolutionsToCheck[mid].width,
+//             "maxHeight": ResolutionsToCheck[mid].height
+//             },
+//             "optional": []
+//         }
+//     }
 
-    navigator.mediaDevices.getUserMedia(temporaryConstraints).then(checkSuccess).catch(checkError);
-}
+//     navigator.mediaDevices.getUserMedia(temporaryConstraints).then(checkSuccess).catch(checkError);
+// }
 
-function checkSuccess(stream)
-{
-    console.log("Success for --> " , mid , " ", ResolutionsToCheck[mid]);
-    selectedWidth = ResolutionsToCheck[mid].width;
-    selectedHeight = ResolutionsToCheck[mid].height;
+// function checkSuccess(stream)
+// {
+//     console.log("Success for --> " , mid , " ", ResolutionsToCheck[mid]);
+//     selectedWidth = ResolutionsToCheck[mid].width;
+//     selectedHeight = ResolutionsToCheck[mid].height;
 
-    left = mid+1;
+//     left = mid+1;
 
-    for (let track of stream.getTracks()) 
-    { 
-        track.stop()
-    }
+//     for (let track of stream.getTracks()) 
+//     { 
+//         track.stop()
+//     }
 
-    FindMaximum_WidthHeight_ForCamera();
-}
+//     FindMaximum_WidthHeight_ForCamera();
+// }
 
-function checkError(error)
-{
-    console.log("Failed for --> " + mid , " ", ResolutionsToCheck[mid],  " ", error);
-    right = mid-1;
-    FindMaximum_WidthHeight_ForCamera();
-}
+// function checkError(error)
+// {
+//     console.log("Failed for --> " + mid , " ", ResolutionsToCheck[mid],  " ", error);
+//     right = mid-1;
+//     FindMaximum_WidthHeight_ForCamera();
+// }
 
-console.log("HEERERERER")
-FindMaximum_WidthHeight_ForCamera();
-console.log("HEERERERER 222")
+// console.log("HEERERERER")
+// FindMaximum_WidthHeight_ForCamera();
+// console.log("HEERERERER 222")
