5281ecc6b9a97bda1e6f7a53cc4f25fc1e14072f
mirmik <mirmikns@yandex.ru>
2023-10-25 21:09:44 +0300

    Some ndi...

--- src/video_track.py
+++ src/video_track.py
@@ -5,9 +5,14 @@ import cv2
 import math
 from av import VideoFrame
 import json
+import random
+import string
 import numpy as np
 import cv2 as cv
 import NDIlib as ndi
+import threading
+import asyncio
+import queue
 
 
 class VideoTransformTrack(MediaStreamTrack):
@@ -23,27 +28,42 @@ class VideoTransformTrack(MediaStreamTrack):
         self.transform = transform
         self.inited = False
         self.ndi_enabled = False
-
         self.send_settings = ndi.SendCreate()
         self.send_settings.ndi_name = 'ndi-python'
         self.ndi_send = ndi.send_create(self.send_settings)
         self.ndi_video_frame = ndi.VideoFrameV2()
+        self.sending_thread = None
+        self.frame_queue = queue.Queue()
 
     def enable_ndi(self, en):
         self.ndi_enabled = en
 
+    def send_frame_to_ndi(self, frame):
+        img = frame.to_ndarray(format="bgr32")
+        #img = cv.cvtColor(img, cv.COLOR_BGR2BGRA)
+        self.ndi_video_frame.data = img
+        #self.ndi_video_frame.FourCC = ndi.FOURCC_VIDEO_TYPE_BGRX
+        self.ndi_video_frame.FourCC = ndi.FOURCC_VIDEO_TYPE_RGBX
+        ndi.send_send_video_v2(self.ndi_send, self.ndi_video_frame)
+
+    def sending_thread_func(self):
+        while True:
+            frame = self.frame_queue.get()
+            if (self.ndi_enabled):
+                self.send_frame_to_ndi(frame)
+
     async def recv(self):
         if (not self.inited):
             self.inited = True
+            #self.sending_thread = threading.Thread(target=self.sending_thread_func)
             print("VideoTransformTrack inited")
 
         frame = await self.track.recv()
         if (self.ndi_enabled):
-            img = frame.to_ndarray(format="bgr24")
-            img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-            self.ndi_video_frame.data = img
-            self.ndi_video_frame.FourCC = ndi.FOURCC_VIDEO_TYPE_BGRX
-            ndi.send_send_video_v2(self.ndi_send, self.ndi_video_frame)
+            if self.sending_thread is None:
+                self.sending_thread = threading.Thread(target=self.sending_thread_func)
+                self.sending_thread.start()
+            self.frame_queue.put(frame)
         return frame
 
 
@@ -51,12 +71,24 @@ class FlagVideoStreamTrack(VideoStreamTrack):
     """
     A video track that returns an animated flag.
     """
+    counter = 0
+
+    def counter_string(self):
+        FlagVideoStreamTrack.counter += 1
+        return str(FlagVideoStreamTrack.counter)
 
     def __init__(self, colors):
         super().__init__()  # don't forget this!
         self.counter = 0
         height, width = 240, 320
         self.label = "LABEL FLAG"
+        
+        self.inited = False
+        self.ndi_enabled = False
+        self.send_settings = ndi.SendCreate()
+        self.send_settings.ndi_name = 'ndi-python-flag' + self.counter_string()
+        self.ndi_send = ndi.send_create(self.send_settings)
+        self.ndi_video_frame = ndi.VideoFrameV2()
 
         # generate flag
         data_bgr = numpy.hstack(
@@ -85,24 +117,42 @@ class FlagVideoStreamTrack(VideoStreamTrack):
         ).transpose()
 
         self.frames = []
+        self.arrs = []
         for k in range(30):
             phase = 2 * k * math.pi / 30
             map_x = id_x + 10 * numpy.cos(omega * id_x + phase)
             map_y = id_y + 10 * numpy.sin(omega * id_x + phase)
-            self.frames.append(
-                VideoFrame.from_ndarray(
-                    cv2.remap(data_bgr, map_x, map_y, cv2.INTER_LINEAR), format="bgr24"
+            arr = cv2.remap(data_bgr, map_x, map_y, cv2.INTER_LINEAR)
+            self.arrs.append(arr)
+            self.frames.append(VideoFrame.from_ndarray(
+                    arr, format="bgr24"
                 )
             )
 
     async def recv(self):
-        pts, time_base = await self.next_timestamp()
+        try:
+            pts, time_base = await self.next_timestamp()
 
-        frame = self.frames[self.counter % 30]
-        frame.pts = pts
-        frame.time_base = time_base
-        self.counter += 1
-        return frame
+            frame = self.frames[self.counter % 30]
+            arr = self.arrs[self.counter % 30]
+            frame.pts = pts
+            frame.time_base = time_base
+            self.counter += 1
+
+            if (self.ndi_enabled):
+                #img = frame.to_ndarray(format="bgr24")
+                #self.ndi_video_frame.data = img
+                #ndi.send_send_video_v2(self.ndi_send, self.ndi_video_frame)
+                #img_bgr = cv.cvtColor(arr, cv.COLOR_RGB2BGR)
+                
+                img = cv.cvtColor(arr, cv.COLOR_BGR2BGRA)
+                self.ndi_video_frame.data =  img
+                self.ndi_video_frame.FourCC = ndi.FOURCC_VIDEO_TYPE_BGRX
+                ndi.send_send_video_v2(self.ndi_send, self.ndi_video_frame)
+                
+            return frame
+        except ex:
+            print("Exception: " + str(ex))
 
     def _create_rectangle(self, width, height, color):
         data_bgr = numpy.zeros((height, width, 3), numpy.uint8)
